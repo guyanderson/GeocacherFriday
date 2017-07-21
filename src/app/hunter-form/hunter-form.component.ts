ccd080b710ed5ad64340fa4799d2194fbb8357a3
guy <luckythelich@hotmail.com>
2017-07-21 13:09:01 -0700

    able to display intended data from API pull via lat/long and address search. Address seach is still hard coded api call.

--- src/app/hunter-form/hunter-form.component.ts
+++ src/app/hunter-form/hunter-form.component.ts
@@ -11,21 +11,22 @@ import { GoogleMapsService } from '../google-maps.service';
 
 export class HunterFormComponent {
 
-   location: any[] = null;
+   address: any[] = null;
+   coordinates: any[] = null;
 
    constructor(private googleService: GoogleMapsService) { }
 
    getAddress(lat: string, long: string) {
       this.googleService.getByLatLong(lat, long).subscribe(response => {
-         this.location = response.json().results[0].formatted_address;
-         console.log(this.location);
+         this.address = response.json().results[0].formatted_address;
+         console.log(this.address);
       });
    }
 
    getCoordinates(number: string, street: string, city: string, state: string, zip: string, country: string) {
       this.googleService.getByAddress(number, street, city, state, zip, country).subscribe(response => {
-         this.location = response.json().results[0].geometry.location.lat;
-         console.log(this.location);
+         this.coordinates = response.json().results[0].geometry.location;
+         console.log(this.coordinates);
       });
    }
 
